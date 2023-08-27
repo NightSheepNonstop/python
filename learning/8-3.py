c72116585f76fed39ea867315c692f9c688c7168
Night Sheep <NightSheepNonstop@outlook.com>
2023-08-27 21:37:04 +0800

    2023/8/27

---
+++ learning/8-3.py
@@ -0,0 +1,6 @@
+def make_shirt(size, words):
+    print(f"The size of this shirt is {size},the words of the shirt are {words}")
+
+
+make_shirt('XL', 'Hello')
+make_shirt(words='Hello', size='XL')
---
+++ learning/8-4.py
@@ -0,0 +1,7 @@
+def make_shirt(size, words='I love Python'):
+    print(f"The size of this shirt is {size},the words of the shirt are {words}")
+
+
+make_shirt('L')
+make_shirt('M')
+make_shirt(size='XXXL', words='THE WORLD')
